39414ebef28d1761a9aa6a1de0b299416b4333e7
MikeFalcon77 <3686170+MikeFalcon77@users.noreply.github.com>
2026-09-13 22:11:45 +0300

    feat(toolkit-utils): exponential backoff for retrying a failed call
    A client that retries immediately is how a brief wobble at the far end
    becomes an outage. This gives callers a delay to wait between attempts
    and a way to work out a deadline up front.
    
    Signed-off-by: MikeFalcon77 <3686170+MikeFalcon77@users.noreply.github.com>

--- libs/toolkit-utils/src/lib.rs
+++ libs/toolkit-utils/src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 #[cfg(feature = "humantime-serde")]
 pub mod humantime_serde;
+pub mod retry_backoff;
 pub mod iso8601_duration;
 pub mod var_expand;
 
